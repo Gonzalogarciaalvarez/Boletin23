2797049f1fe88dc1fd740853ef4a71884b73ba08
ggarciaalvarez <ggarciaalvarez@victoria26.danielcastelao.org>
2018-04-12 13:54:22 +0200

    Para subir

---
+++ src/boletin23/Boletin23.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin23;
+
+/**
+ *
+ * @author ggarciaalvarez
+ */
+public class Boletin23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        //Boletin23_1 obx = new Boletin23_1();
+        //Boletin23_2 obx = new Boletin23_2();
+        //Boletin23_3 obx = new Boletin23_3();
+        Boletin23_4 obx= new Boletin23_4();
+    }
+    
+}
